1f9c01e67944bd26734bc7902b58dbf25837a805
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-16 18:21:38 -0500

    pokemon now have weight property just for fun, also added a method to get pokeom by id

--- Trainer.js
+++ Trainer.js
@@ -1,6 +1,7 @@
 class Pokemon {
     constructor(name) {
         this.name = name
+        this.weight = 0
         this.hp = 0
         this.attack = 0
         this.defense = 0
@@ -20,6 +21,7 @@ class Trainer {
             if (req.readyState === 4) {
                 let acquiredPokemon = JSON.parse(req.responseText)
                 let pokemonToAdd = new Pokemon(name)
+                pokemonToAdd.weight = acquiredPokemon.weight
                 pokemonToAdd.hp = acquiredPokemon.stats[5].base_stat
                 pokemonToAdd.attack = acquiredPokemon.stats[4].base_stat
                 pokemonToAdd.defense = acquiredPokemon.stats[3].base_stat
@@ -35,6 +37,30 @@ class Trainer {
             
         }
         req.open('GET', `https://fizal.me/pokeapi/api/v2/name/${name}.json`)
+        req.send()
+    }
+    addPokemonById(id) {
+        let req = new XMLHttpRequest()
+        req.onreadystatechange = _ => {
+            if (req.readyState === 4) {
+                let acquiredPokemon = JSON.parse(req.responseText)
+                let pokemonToAdd = new Pokemon(acquiredPokemon.name)
+                pokemonToAdd.weight = acquiredPokemon.weight
+                pokemonToAdd.hp = acquiredPokemon.stats[5].base_stat
+                pokemonToAdd.attack = acquiredPokemon.stats[4].base_stat
+                pokemonToAdd.defense = acquiredPokemon.stats[3].base_stat
+                for (let a in acquiredPokemon.abilities){
+                    let ability = acquiredPokemon.abilities[a].ability.name
+                    ability.replace('-', ' ').charAt(0).toUpperCase()
+                    pokemonToAdd.abilities.push(ability)
+                }
+                this.pokemonList.push(pokemonToAdd)
+                return true
+            }
+            
+            
+        }
+        req.open('GET', `https://fizal.me/pokeapi/api/v2/name/${id}.json`)
         req.send()
     }
 
