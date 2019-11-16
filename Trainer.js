77bc5146221bbcefe526cbdd5fac48ccc8272929
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-16 17:59:28 -0500

    Created classes for pokemon and trainer

---
+++ Trainer.js
@@ -0,0 +1,20 @@
+class Pokemon {
+    constructor(name) {
+        this.name = name
+        this.hp = 0
+        this.attack = 0
+        this.defense = 0
+        this.abilities = []
+    }
+
+}
+
+class Trainer {
+    constructor(name) {
+        this.name = name
+        this.pokemonList = []
+    }
+    
+
+}
+
