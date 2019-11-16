775cdea0afbebefcec54241cd67818e10cbce435
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-16 18:00:36 -0500

    gave trainer a method to add Pokemon by their names

--- Trainer.js
+++ Trainer.js
@@ -14,7 +14,27 @@ class Trainer {
         this.name = name
         this.pokemonList = []
     }
-    
+    addPokemonByName(name) {
+        let req = new XMLHttpRequest()
+        req.onreadystatechange = _ => {
+            if (req.readyState === 4) {
+                let acquiredPokemon = JSON.parse(req.responseText)
+                let pokemonToAdd = new Pokemon(name)
+                
+                for (let a in acquiredPokemon.abilities){
+                    let ability = acquiredPokemon.abilities[a].ability.name
+                    ability.replace('-', ' ').charAt(0).toUpperCase()
+                    pokemonToAdd.abilities.push(ability)
+                }
+                this.pokemonList.push(pokemonToAdd)
+                return true
+            }
+            
+            
+        }
+        req.open('GET', `https://fizal.me/pokeapi/api/v2/name/${name}.json`)
+        req.send()
+    }
 
 }
-
+let michel = new Trainer('Michel')
