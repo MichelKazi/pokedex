df068ab8929c0164a6d6ad79c5a73f8efb3d6841
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-18 00:21:05 -0500

    the api call for pokemon info is now done using axios in a separate method which returns a parsed JSON

--- src/Trainer.js
+++ src/Trainer.js
@@ -1,3 +1,5 @@
+import Axios from 'axios'
+
 class Pokemon {
     constructor(name, stats) {
         this.name = name
@@ -17,30 +19,40 @@ class Trainer {
         return this.pokemonList
     }
     addPokemon(input) {
-        const req = new XMLHttpRequest()
-        req.onreadystatechange = _ => {
-            if (req.readyState === 4 && req.status === 200) {
-                const acquiredPokemon = JSON.parse(req.responseText)
-                const stats = {
-                    weight : acquiredPokemon.weight,
-                    hp : acquiredPokemon.stats[5].base_stat, // For every single Pokemon JSON, 
-                    attack : acquiredPokemon.stats[4].base_stat, // these indices for stats will
-                    defense : acquiredPokemon.stats[3].base_stat, // always be the same 
-                    abilities : acquiredPokemon.abilities.map( abilities => {
-                        return abilities.ability.name.replace('-', ' ')
-                    })    
-                }// stats is an object literal that is meant to be the second parameter for Pokemon class
-                // I can create new keys and values right in this object and assign information from the 
-                // JSON file to them
-                const pokemonToAdd = new Pokemon(acquiredPokemon.name, stats)
-                pokemonToAdd.spriteURL = acquiredPokemon.sprites.front_default
-                this.pokemonList.push(pokemonToAdd)
-            } else if (req.status === 404) console.log(`${input} was not a valid entry!`) 
+        const acquiredPokemon = this.fetchPokeAPI(input)
+        const stats = {
+            weight : acquiredPokemon.weight,
+            hp : acquiredPokemon.stats[5].base_stat, // For every single Pokemon JSON, 
+            attack : acquiredPokemon.stats[4].base_stat, // these indices for stats will
+            defense : acquiredPokemon.stats[3].base_stat, // always be the same 
+            abilities : acquiredPokemon.abilities.map( abilities => {
+                return abilities.ability.name.replace('-', ' ')
+            })    
         }
-        isNaN(input) ? req.open('GET', `https://fizal.me/pokeapi/api/v2/name/${input}.json`) : req.open('GET', `https://fizal.me/pokeapi/api/v2/id/${input}.json`)
-        req.send()
+        const pokemonToAdd = new Pokemon(acquiredPokemon.name, stats)
+        pokemonToAdd.spriteURL = acquiredPokemon.sprites.front_default
+        this.pokemonList.push(pokemonToAdd)
+
         return isNaN(input) ? `${input} was added to ${this.name}'s list of pokemon!` : `Pokemon with ID: ${input} was added to ${this.name}'s list of pokemon!`
+    }
+
+    fetchPokeAPI(input) {
+        const axios = require('axios');
+        // Make a request for a user with a given ID
+        isNaN(input) ? axios.get(`https://fizal.me/pokeapi/api/v2/name/${input}.json`) : axios.get(`https://fizal.me/pokeapi/api/v2/id/${input}.json`)
+            .then(function (response) {
+                // handle success
+                const acquired = response.data
+                console.log(response);
+                return acquired
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(`${input} was not a valid entry!`)
+                console.log(error);
+            })
     }
 
 }
 
+export default Trainer
