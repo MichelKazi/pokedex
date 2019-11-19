b59cf8c73c667a65875c3b5ea6ef8c9bbaa6c51b
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-18 22:25:45 -0500

    Updates

---
+++ precache-manifest.df40373a0bfc7db9199c4a81b994d8cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995573711f72c7f9956668ef8935aab0",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  },
+  {
+    "revision": "0a695d038f39d8a77e87",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.b6798f4c.chunk.css"
+  },
+  {
+    "revision": "13412c30a2e140092d0d",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.21252012.chunk.js"
+  },
+  {
+    "revision": "0a695d038f39d8a77e87",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.5459faf8.chunk.js"
+  },
+  {
+    "revision": "d1f8d3cd6b9d93f7c334",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime-main.aa2e2c54.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TD-React-Challenge-Test-1/static/media/logo.25bf045c.svg"
+  }
+]);
