a0ebb4eceb56663c8e1f016adec83fed1f4827d3
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-20 22:47:19 -0500

    Pokemon doesn't have a nested object anymore

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -28,18 +28,18 @@ export class Pokemon {
         try {
             response = await axios.get(`https://fizal.me/pokeapi/api/v2/${isNaN(id) ? 'name/' : 'id/'}${id}.json`)
             const acquiredPokemon = response.data
-            const stats = {
-                weight: acquiredPokemon.weight,
-                hp: acquiredPokemon.stats[5].base_stat, // For every single Pokemon JSON, 
-                attack: acquiredPokemon.stats[4].base_stat, // these indices for stats will
-                defense: acquiredPokemon.stats[3].base_stat, // always be the same 
-                abilities: acquiredPokemon.abilities.map(abilities => {
-                    return abilities.ability.name.replace('-', ' ')
-                })
-            }
-            this.stats = stats
+            
             this.name = acquiredPokemon.name
             this.spriteURL = acquiredPokemon.sprites.front_default
+            this.weight = acquiredPokemon.weight
+            this.hp = acquiredPokemon.stats[5].base_stat// For every single Pokemon JSON, 
+            this.attack = acquiredPokemon.stats[4].base_stat // these indices for stats will
+            this.defense = acquiredPokemon.stats[3].base_stat // always be the same 
+            
+            this.abilities = acquiredPokemon.abilities.map(abilities => {
+                return abilities.ability.name.replace('-', ' ')
+            })
+            
             return this
         }
         catch (err) {
