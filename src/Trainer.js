9ba06ec16791164ff335afcbc65835fd607b3b59
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-23 11:25:15 -0500

    updated trainer.js more

--- src/Trainer.js
+++ src/Trainer.js
@@ -2,35 +2,48 @@ import Pokemon from './Pokemon'
 
 
 export class Trainer {
-    constructor(name) {
-        this.name = name
-        this.pokemonList = []
+  constructor(name) {
+    this.name = name
+    this.pokemonList = []
+    this.team = []
 
-        //binding this keyword for methods
-        this.all = this.all.bind(this)
-        this.addPokemon = this.addPokemon.bind(this)
-        this.get = this.get.bind(this)
+    //binding this keyword for methods
+    this.all = this.all.bind(this)
+    this.addPokemon = this.addPokemon.bind(this)
+    this.get = this.get.bind(this)
+  }
+
+  all() {
+    return this.pokemonList
+  }
+
+  allTeam(){
+    return this.team
+  }
+
+  addPokemon(pokemon) {
+    this.pokemonList.push(pokemon)
+  }
+
+
+  removePokemon(pokemon) {
+    if(this.get(pokemon))
+      this.pokemonList = this.pokemonList.splice(this.pokemonList.indexOf(pokemon))
+  }
+
+  get(pokemon) {
+    if (this.pokemonList.includes(pokemon))
+      return this.pokemonList[this.pokemonList.indexOf(pokemon)]
+    return null
+  }
+
+  equip(pokemon){
+    if (this.pokemonList.includes(pokemon) && this.team.length <4){
+      this.team.push(pokemon)
+      return this.team
     }
-
-    all () {
-        return this.pokemonList
-    }
-
-    addPokemon(pokemon) {
-        this.pokemonList.push(pokemon)
-    }
-
-    get(pokemon){
-        if(this.pokemonList.includes(pokemon))
-            return this.pokemonList[this.pokemonList.indexOf(pokemon)]
-        return `You don't have this Pokemon`
-    }
-
-    removePokemon(input) {
-        // eslint-disable-next-line eqeqeq
-        this.pokemonList = this.pokemonList.filter(pokemon => isNaN(input) ? pokemon.id!=input : pokemon.name!=input)
-        //this line doesn't work :(
-    }
-
+    else console.error(`Your team is full!`);
+    
+  }
 }
 export default Trainer
